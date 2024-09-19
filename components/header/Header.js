dbd35c040f8cb613b637e2fa2e75bfc2bff1ef6f
Marco Di Summa <mdisumma@yahoo.com>
2024-09-19 21:55:02 +0100

    css header

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Rubik_Bubbles } from "next/font/google";
 
 const fontTitle = Rubik_Bubbles({ weight: "400", subsets: ["latin"] });
@@ -5,7 +6,9 @@ const fontTitle = Rubik_Bubbles({ weight: "400", subsets: ["latin"] });
 export default function Header() {
   return (
     <header className="header">
-      <h1 className={fontTitle.className}>Pawfect Companion</h1>
+      <div className="header-container">
+        <h1 className={fontTitle.className}>Pawfect Companion</h1>
+      </div>
       <p>
         Unleash a Lifetime of Joy by Building an Unbreakable Bond with Your Best
         Friend, Creating a Happier and More Enjoyable Life for Both of You!
