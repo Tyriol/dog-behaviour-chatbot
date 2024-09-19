99aec653e09999e04c1d9d54adacd6247ef70ffe
Ryan <ryanshaunsmith@gmail.com>
2024-09-19 13:46:52 +0100

    add app name and start input form

--- components/text.js
+++ components/text.js
@@ -1,3 +1,16 @@
 export default function Test() {
-  return <h1>Test</h1>;
+  return (
+    <>
+      <form>
+        <label>
+          How can we help?
+          <input />
+        </label>
+        <button type="submit"></button>
+      </form>
+      <section className="response-display">
+        <p></p>
+      </section>
+    </>
+  );
 }
